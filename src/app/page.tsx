ea5dbe6b90282e60652a1ff2be8754e18cf033ea
Tomjid huda <tomjidhuda04@gmail.com>
2025-11-05 13:07:16 +0600

    feat: Add empty state to home page
    This commit introduces an empty state to the home page, which is displayed when there are no chat rooms available. It also includes a call to action to create a new room.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty";
+import { MessageSquareIcon } from "lucide-react";
+import Link from "next/link";
 
 const HomePage = () => {
-  return null;
+  return (
+    <div className="container mx-auto max-w-3xl px-4 py-8 space-y-8">
+      <Empty className="border border-dashed">
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <MessageSquareIcon />
+          </EmptyMedia>
+        </EmptyHeader>
+        <EmptyTitle>No Chat Rooms</EmptyTitle>
+        <EmptyDescription>
+          Create a new chat room to get started
+        </EmptyDescription>
+        <EmptyContent>
+          <Button asChild>
+            <Link href="rooms/new">Create Room</Link>
+          </Button>
+        </EmptyContent>
+      </Empty>
+    </div>
+  );
 };
 
 export default HomePage;
